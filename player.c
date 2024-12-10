f30134017f7492fcab78579868aef735f2ba140b
최수림 <choi93864320@gmail.com>
2024-12-10 20:12:09 +0900

    Update player.c

--- player.c
+++ player.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "player.h"
 
-// 플레이어 정보를 입력받는 함수
 void input_players(int p, int level[], char name[][17]) {
     for (int i = 0; i < p; i++) {
         scanf("%d %s", &level[i], name[i]);
